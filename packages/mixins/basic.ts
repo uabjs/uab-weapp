158dcf4132e10b4b9998be237ffb00040b2212a9
shunyue <1277952981@qq.com>
2023-10-11 18:09:39 +0800

    chore: 公用组件添加类似 vue 的 mixins 混入

---
+++ packages/mixins/basic.ts
@@ -0,0 +1,21 @@
+
+// 默认注入到组件内的共享特性，类似于一些编程语言中的 “mixins” 或 “traits”。
+export const basic = Behavior({
+  methods: {
+    $emit(
+      name: string,
+      detail?: Record<string, unknown>,
+      options?: WechatMiniprogram.Component.TriggerEventOption
+    ) {
+      // 微信小程序组件间通信（参考：https://developers.weixin.qq.com/miniprogram/dev/framework/custom-component/events.html）
+      this.triggerEvent(name, detail, options);
+    },
+
+    // 设置 data 数据，返回一个 promise 微任务，页面数据改变后会触发 promise 的成功回调
+    set(data: Record<string, unknown>) {
+      this.setData(data);
+
+      return new Promise((resolve) => wx.nextTick(resolve));
+    },
+  },
+});
