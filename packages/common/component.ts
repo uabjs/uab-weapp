b46e5171569c9bb106c5670bde325f8d557d00f5
shunyue <1277952981@qq.com>
2023-10-12 14:20:19 +0800

    chore: 给小程序组件添加 vue 属性的映射，这样就可以用 vue 的语法写小程序的组件了

--- packages/common/component.ts
+++ packages/common/component.ts
@@ -1,12 +1,38 @@
 import { UabComponentOptions } from '../definitions'
 import { basic } from '../mixins/basic'
 
+function mapKeys(
+  source: Record<string, any>,
+  target: Record<string, any>,
+  map: Record<string, any>
+) {
+  Object.keys(map).forEach((key) => {
+    if (source[key]) {
+      target[map[key]] = source[key]
+    }
+  })
+}
+
 function UabComponent<
   Data extends WechatMiniprogram.Component.DataOption,
   Props extends WechatMiniprogram.Component.PropertyOption,
   Methods extends WechatMiniprogram.Component.MethodOption
 >(uabOptions: UabComponentOptions<Data, Props, Methods>): void {
   const options: WechatMiniprogram.Component.Options<Data, Props, Methods> = {};
+
+  // 给小程序组件添加 vue 属性的映射，这样就可以用 vue 的语法写小程序的组件了 （别问为什么微信小程序不和 vue组件 options 保持统一，程序员都有一颗逆袭的叛逆心）
+  mapKeys(uabOptions, options, {
+    data: 'data',
+    props: 'properties',
+    watch: 'observers',
+    mixins: 'behaviors',
+    methods: 'methods',
+    beforeCreate: 'created',
+    created: 'attached',
+    mounted: 'ready',
+    destroyed: 'detached',
+    classes: 'externalClasses',
+  })
 
   // 设置 externalClasses 外部样式类的默认值 （查考：https://developers.weixin.qq.com/miniprogram/dev/framework/custom-component/wxml-wxss.html#%E5%A4%96%E9%83%A8%E6%A0%B7%E5%BC%8F%E7%B1%BB）
   options.externalClasses = options.externalClasses || []
